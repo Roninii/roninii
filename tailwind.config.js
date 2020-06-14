2879122cf0c5a186454427713187831511edd46c
Ronnie Villarini <Villarini91@gmail.com>
2020-06-14 17:34:14 -0500

    extend tailwind color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        "primary":" #6200ee",
+        "primary-dark":" #5d3991",
+        "secondary":" #04dac6",
+      }
+    }
+  },
   variants: {},
   plugins: []
 }
