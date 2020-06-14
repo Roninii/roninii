c99c31ae6c75fd2b782ebd3619d34b6d556d01b6
Ronnie Villarini <Villarini91@gmail.com>
2020-06-14 18:21:43 -0500

    add index skeleton

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,34 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        ronini
-      </h1>
-      <h2 class="subtitle">
-        Personal site of Ronnie Villarini
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <article class="grid gap-6 md:w-3/4">
+      <section>
+        <h2 class="text-5xl text-gray-800">
+          Hi! My name is
+          <span class="text-primary">Ron</span>nie Villar<span
+            class="text-primary"
+            >ini</span
+          >
+        </h2>
+        <p class="text-gray-600 text-lg">
+          I'm a full stack developer from Mattoon, Illinois.
+        </p>
+      </section>
+      <h3 class="text-3xl text-gray-800">
+        I breathe life into your business and build software your users will
+        actually enjoy using.
+      </h3>
+    </article>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
+export default {}
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
 .container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  display: grid;
+  place-items: center center;
+  @apply min-h-screen px-8 py-4;
 }
 </style>
