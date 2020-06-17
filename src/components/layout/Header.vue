853a31b868d1098e44662077337c7a53862a63e7
Ronnie Villarini <Villarini91@gmail.com>
2020-06-17 11:21:38 -0500

    add page accent gradient

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,43 @@
+<template>
+    <header class="uppercase font-medium tracking-wide flex justify-between">
+        <h1 class="text-primary">
+            Ronini
+        </h1>
+
+        <nav>
+            <ul class="grid md:grid-flow-col gap-4">
+                <li>
+                    <g-link active-class="text-primary" class="text-gray-700" to="/">Home</g-link>
+                </li>
+                <li>
+                    <g-link active-class="text-primary" class="text-gray-700" to="/about"
+                        >About</g-link
+                    >
+                </li>
+                <li>
+                    <g-link active-class="text-primary" class="text-gray-700" to="/portfolio"
+                        >My Work</g-link
+                    >
+                </li>
+                <li>
+                    <g-link active-class="text-primary" class="text-gray-700" to="/contact"
+                        >Contact</g-link
+                    >
+                </li>
+                <li>
+                    <g-link active-class="text-primary" class="text-gray-700" to="/blog"
+                        >Blog</g-link
+                    >
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+};
+</script>
+
+<style scoped></style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,16 +1,8 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
-    <slot/>
-  </div>
+    <div class="bg-gray-200 min-h-screen rounded">
+        <Header />
+        <slot />
+    </div>
 </template>
 
 <static-query>
@@ -21,30 +13,13 @@ query {
 }
 </static-query>
 
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
+<script>
+import Header from '~/components/layout/Header.vue';
 
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
-</style>
+export default {
+    name: 'Layout',
+    components: {
+        Header,
+    },
+};
+</script>
