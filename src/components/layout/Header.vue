1b01b0882baf42cc92bcaafc6dc2a6e242518ce9
Ronnie Villarini <Villarini91@gmail.com>
2020-06-18 16:43:07 -0500

    add toggleable nav for mobile layout

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,37 +1,50 @@
 <template>
-    <header class=" py-4 uppercase text-lg font-medium tracking-wide flex justify-between">
+    <header class="py-4 uppercase text-lg font-medium tracking-wide flex justify-between">
         <h1 class="text-primary">
-            <g-link to="/">
-                Ronini.dev
-            </g-link>
+            <g-link to="/">Ronini.dev</g-link>
         </h1>
 
-        <nav>
-            <ul class="grid sm:grid-flow-col gap-4 text-gray-600">
+        <nav class="text-right relative">
+            <NavButton :menu="!navHidden" @click="navHidden = !navHidden" class="-mr-4 sm:hidden" />
+            <ul
+                :class="{'hidden': navHidden}"
+                class="absolute right-0 sm:relative sm:grid sm:grid-flow-col gap-4 text-gray-600 text-right"
+            >
                 <li>
-                    <g-link class="hover:text-primary relative z-10" active-class="text-primary" to="/" exact
-                        >Home</g-link
-                    >
+                    <g-link
+                        class="hover:text-primary relative z-10"
+                        active-class="text-primary"
+                        to="/"
+                        exact
+                    >Home</g-link>
                 </li>
                 <li>
-                    <g-link class="hover:text-primary relative z-10" active-class="text-primary" to="/about"
-                        >About</g-link
-                    >
+                    <g-link
+                        class="hover:text-primary relative z-10"
+                        active-class="text-primary"
+                        to="/about"
+                    >About</g-link>
                 </li>
                 <li>
-                    <g-link class="hover:text-primary relative z-10" active-class="text-primary" to="/portfolio"
-                        >My Work</g-link
-                    >
+                    <g-link
+                        class="hover:text-primary relative z-10"
+                        active-class="text-primary"
+                        to="/portfolio"
+                    >My Work</g-link>
                 </li>
                 <li>
-                    <g-link class="hover:text-primary relative z-10" active-class="text-primary" to="/contact"
-                        >Contact</g-link
-                    >
+                    <g-link
+                        class="hover:text-primary relative z-10"
+                        active-class="text-primary"
+                        to="/contact"
+                    >Contact</g-link>
                 </li>
                 <li>
-                    <g-link class="hover:text-primary relative z-10" active-class="text-primary" to="/blog"
-                        >Blog</g-link
-                    >
+                    <g-link
+                        class="hover:text-primary relative z-10"
+                        active-class="text-primary"
+                        to="/blog"
+                    >Blog</g-link>
                 </li>
             </ul>
         </nav>
@@ -41,6 +54,14 @@
 <script>
 export default {
     name: 'Header',
+    components: {
+        NavButton: () => import('./NavButton.vue'),
+    },
+    data() {
+        return {
+            navHidden: true,
+        };
+    },
 };
 </script>
 
@@ -50,11 +71,11 @@ a::before {
     height: 2px;
     z-index: -1;
     bottom: 0;
-    background-image: linear-gradient(to right,#6200ee, #04dac6);
+    background-image: linear-gradient(to right, #6200ee, #04dac6);
     background-position: 50% 50%;
     background-repeat: no-repeat;
     background-size: 0% 100%;
-    @apply absolute ;
+    @apply absolute;
     @apply transition-all;
     @apply duration-200;
     @apply ease-linear;
