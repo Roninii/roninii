cf0a6cb0ec9441340d95a21db4375025d993c3cf
Ronnie Villarini <Villarini91@gmail.com>
2020-06-18 10:38:09 -0500

    add page skeletons

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>About us</h1>
+        <h1>Hi! My name is Ronnie Villarini.</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
     </div>
 </template>
@@ -8,7 +8,7 @@
 <script>
 export default {
     metaInfo: {
-        title: 'About us',
+        title: 'About',
     },
 };
 </script>
---
+++ src/pages/Contact.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Get in touch and let's build something together!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Contact',
+};
+</script>
+
+<style scoped>
+</style>
