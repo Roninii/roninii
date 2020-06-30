872a312c9abbed2eb6ef440e5ef760ce31c3aaa1
Ronnie Villarini <Villarini91@gmail.com>
2020-06-30 08:18:13 -0500

    add contact skeleton

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,6 +1,20 @@
 <template>
-    <div>
-        <h1>Get in touch and let's build something together!</h1>
+    <div class="min-h-screen grid md:grid-cols-2 gap-4">
+        <h1 class="text-4xl uppercase font-medium tracking-wide md:col-span-2">
+            Let's build something
+            <span class="text-primary">together</span>!
+        </h1>
+        <form>
+            <input type="text" class="border-2 border-primary rounded bg-transparent" />
+            <input type="email" class="border-2 border-primary rounded bg-transparent" />
+            <textarea
+                name
+                id
+                cols="40"
+                rows="5"
+                class="border-2 border-primary rounded bg-transparent md:col-span-2"
+            ></textarea>
+        </form>
     </div>
 </template>
 
