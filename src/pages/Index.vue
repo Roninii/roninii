5b18e970b6467d483d0facf7a942dd100f64f925
Ronnie Villarini <Villarini91@gmail.com>
2020-06-30 08:17:56 -0500

    abstract hero

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,22 +1,16 @@
 <template>
-    <article class="grid gap-6 md:w-2/3 mx-auto">
-        <section>
-            <h2 class="text-6xl leading-none sm:leading-10">
-                <span class="text-primary">Ron</span>nie Villar<span class="text-primary">ini</span>
-            </h2>
-            <p class="text-gray-600 text-lg leading-none sm:leading-10">Full stack developer</p>
-        </section>
-        <h3 class="text-3xl sm:w-2/3">
-            I breathe life into your business online and build software your users will actually enjoy
-            using.
-        </h3>
-    </article>
+    <div>
+        <Hero />
+    </div>
 </template>
 
 <script>
 export default {
     metaInfo: {
         title: 'Home',
+    },
+    components: {
+        Hero: () => import('~/components/index/Hero.vue'),
     },
 };
 </script>
