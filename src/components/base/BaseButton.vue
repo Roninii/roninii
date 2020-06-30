1327cc478848cdeb956c04abbc359e3ae58808ee
Ronnie Villarini <Villarini91@gmail.com>
2020-06-30 09:34:49 -0500

    add styled button wrapper

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <button
+        class="action button bg-primary px-4 py-2 my-8 uppercase font-medium rounded"
+        v-on="$listeners"
+    >{{ text }}</button>
+</template>
+
+<script>
+export default {
+    name: 'BaseButton',
+    inheritAttrs: false,
+    props: {
+        text: {
+            type: String,
+            default: 'Submit',
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
