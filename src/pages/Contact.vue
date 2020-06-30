18b64483d924af1e83541769ccb565e9a19f86e5
Ronnie Villarini <Villarini91@gmail.com>
2020-06-30 15:20:06 -0500

    layout tweak

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,21 +1,23 @@
 <template>
-    <div class="min-h-screen grid md:grid-cols-2 gap-4 items-center">
-        <h1 class="text-4xl text-center uppercase font-medium tracking-wide md:col-span-2">
-            Let's build something
-            <span class="text-primary">together</span>!
-        </h1>
-        <form class="md:col-span-2 md:w-1/3 mx-auto grid md:grid-cols-2 col-gap-2 row-gap-4">
-            <BaseInput type="text" name="name" />
-            <BaseInput type="email" name="email" />
-            <textarea
-                name="services description"
-                id
-                cols="40"
-                rows="5"
-                class="rounded bg-transparent md:col-span-2 border border-primary focus:outline-none"
-            ></textarea>
-            <BaseButton class="col-span-2" text="Submit" @click.prevent="success = true" />
-        </form>
+    <div class="min-h-screen grid items-center">
+        <div class="grid md:grid-cols-2 gap-4 items-center">
+            <h1 class="text-4xl text-center uppercase font-medium tracking-wide md:col-span-2">
+                Let's build something
+                <span class="text-primary">together</span>!
+            </h1>
+            <form class="md:col-span-2 md:w-1/3 mx-auto grid md:grid-cols-2 col-gap-2 row-gap-4">
+                <BaseInput type="text" name="name" />
+                <BaseInput type="email" name="email" />
+                <textarea
+                    name="services description"
+                    id
+                    cols="40"
+                    rows="5"
+                    class="rounded bg-transparent md:col-span-2 border border-primary focus:outline-none"
+                ></textarea>
+                <BaseButton class="col-span-2" text="Submit" @click.prevent="success = true" />
+            </form>
+        </div>
     </div>
 </template>
 
